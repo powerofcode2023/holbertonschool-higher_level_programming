db47611743c1c26ed436154c54e35837d3c68446
abdel <holbertonc21@gmail.com>
2023-10-03 03:10:51 -0700

    fixed pycodestyle 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -7,6 +7,6 @@ def new_in_list(my_list, idx, element):
     else:
         new_list = []
         for i in my_list:
-        new_list.append(i)
+            new_list.append(i)
         new_list[idx] = element
         return new_list
