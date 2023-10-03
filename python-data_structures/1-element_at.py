ea96be688d84558518258a85e8dabbed80fe6b3e
abdel <holbertonc21@gmail.com>
2023-10-03 01:06:00 -0700

    modfied script 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -6,12 +6,3 @@ def element_at(my_list, idx):
         return None
     else:
         return my_list[idx]
-#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list) - 1:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
