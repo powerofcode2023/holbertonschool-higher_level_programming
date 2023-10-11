5316cd2cffe764af3d28bcf00ae134e66f304072
abdel <holbertonc21@gmail.com>
2023-10-11 00:08:37 -0700

    created script 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define square class"""
+
+
+class Square:
+    """define method of Square"""
+
+    def __init__(self, size=0):
+        """size of the square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
