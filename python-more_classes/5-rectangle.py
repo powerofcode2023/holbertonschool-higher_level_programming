ff83a8116042aa2028516e2865d588434aaf32c4
abdel <holbertonc21@gmail.com>
2023-10-13 10:34:12 -0700

    created script 5-rectangle.py

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""5-rectangle.py:
+    print a rectangle
+"""
+
+
+class Rectangle:
+    """class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return ("#" * self.__width + "\n") * (self.__height - 1)\
+            + "#" * self.__width
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__height, self.__width)
+
+    def __del__(self):
+        print("Bye rectangle...")
