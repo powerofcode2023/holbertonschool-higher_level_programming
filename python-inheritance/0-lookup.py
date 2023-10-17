a6adb3792c2ab21ccc31f660111f69421136819d
abdel <holbertonc21@gmail.com>
2023-10-17 08:17:01 -0700

    created script 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""a function that returns the list of available attributes"""
+"""and methods of an object:"""
+
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
