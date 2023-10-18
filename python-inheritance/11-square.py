0905ba7f35d27937fc2f7b9b27aaff78d52b06bd
abdel <holbertonc21@gmail.com>
2023-10-18 08:35:31 -0700

    created script 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""define class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class for square  that inherit from BaseGeometry"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """return area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string representation of Square"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)i
