2d5678a6ce044c927c50f2f0609ea13f55641b45
abdel <holbertonc21@gmail.com>
2023-10-18 07:36:05 -0700

    created script 9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""define class Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class for rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """string representation of Rectangle"""
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
