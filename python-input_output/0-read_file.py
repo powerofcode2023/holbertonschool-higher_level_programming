6482034def61b4b9fbb1a62e349e4b8b26ca8794
root <holberton2023c21@gmail.com>
2023-10-20 05:51:21 -0700

    fixed pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define function to read text file and prints to stdout"""
 
+
 def read_file(filename=""):
     """Read a text file and print to stdout"""
     with open(filename, encoding='utf-8') as file:
