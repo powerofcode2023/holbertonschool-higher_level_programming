ac22731843b39d697ada4372ecbda6abe9b5d122
abdel <holbertonc21@gmail.com>
2023-10-24 09:24:55 +0200

    created script base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define Base class"""
+
+class Base:
+    """manage id attribute for classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize to given id or
+        increase class nb_objects and set as default id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
