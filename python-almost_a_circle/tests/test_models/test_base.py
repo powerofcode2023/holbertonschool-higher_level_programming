fe958ef11d398daf8a35a6a696227e97f49395b3
abdel <holbertonc21@gmail.com>
2023-10-26 11:24:34 +0200

    added unit tests test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -19,6 +19,21 @@ class TestBaseClass(unittest.TestCase):
         b4 = Base()
         self.assertEqual(b4.id, 3)
 
+    def test_to_json_string_None(self):
+        """Test if to_json_string method handles None input."""
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_to_json_string_empty(self):
+        """Test if to_json_string method handles empty input."""
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_from_json_string_None(self):
+        """Test if from_json_string method handles None input."""
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty(self):
+        """Test if from_json_string method handles empty input."""
+        self.assertEqual(Base.from_json_string(""), [])
 
 if __name__ == "__main__":
     unittest.main()
