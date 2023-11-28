14294bf20af17c57b82fc86081300c127e3d3622
powerofcode2023 <holbertonc21@gmail.com>
2023-11-28 12:20:21 +0100

    fixed code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,23 +5,23 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    # Recover argument from user
+    """# Recover argument from user"""
     user = sys.argv[1]
     pswd = sys.argv[2]
     db_name = sys.argv[3]
 
-    # connect database
+    """connect database"""
     db = MySQLdb.connect(host='localhost', user=user,
                          passwd=pswd, db=db_name, port=3306)
 
     # create cursor
     cursor = db.cursor()
 
-    # executing MySQL Queries in Python
+    """executing MySQL Queries in Python"""
     cursor.execute("SELECT * FROM states WHERE states.name \
                 LIKE BINARY 'N%' ORDER BY states.id ASC")
 
-    # display
+    """display"""
     all_statesN = cursor.fetchall()
     for row in all_statesN:
         print(row)
