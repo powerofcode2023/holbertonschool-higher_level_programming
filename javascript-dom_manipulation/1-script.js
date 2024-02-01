144ec13b2d8fbf3bf7983c5f788cfb31b14a410f
abdel <holbertonc21@gmail.com>
2024-02-01 10:31:25 +0100

    fixed style

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,3 +1,3 @@
-document.querySelector('#red_header').onclick = function (){
-    document.querySelector('header').style.color = "#FF0000";
-}
+document.querySelector('#red_header').onclick = function () {
+  document.querySelector('header').style.color = '#FF0000';
+};
